df868e6b70b068b2cdcee0e5bc4a87cb5f960170
rath-prak <rathanap@gmail.com>
2016-07-18 21:00:46 +1200

    add scrollmagic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
 
-	// ScrollMagic code will go here
+	// variables
+	var controller;
 
-});
+	// initialize ScrollMagic controller
+	controller = new ScrollMagic.Controller();
+
+	// scene 1 - pin our main section
+	var pinScene01 = new ScrollMagic.Scene({
+		triggerElement: '#main', 
+		triggerHook: 0, 
+		duration: '900%'
+	})
+	.setPin('#main .pin-wrapper', {pushFollowers: false})
+	.addTo(controller);
+
+
+}); // end of document ready
+
+
+// '.setPin()' is a special scrollmagic method 
